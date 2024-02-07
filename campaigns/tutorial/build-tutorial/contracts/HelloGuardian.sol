828e043a22f8d20fca6f47da89d0206dccdae16e
zuberOne <zuberluk@proton.me>
2024-02-07 23:28:47 +0100

    first steps

--- campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
+++ campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
@@ -5,7 +5,7 @@ contract HelloGuardian {
 
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
-        // CODE HERE
+        return("Hello Guardian");
     }
 
 }
