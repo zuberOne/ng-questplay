fd385144ad276a17432538f57cfac68efde4e483
zuberOne <zuberluk@proton.me>
2024-02-08 20:41:50 +0100

    ctf tutorial

--- campaigns/tutorial/ctf-tutorial/script/clearcamp.js
+++ campaigns/tutorial/ctf-tutorial/script/clearcamp.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 // Input bandit camp address here
-const BANDIT_CAMP_ADDRESS = 0x0
+const BANDIT_CAMP_ADDRESS = "0xA838F779584a7AbA47c7e87DbB3d7280C45859c7"
 
 async function main() {
   const camp = await ethers.getContractAt(
