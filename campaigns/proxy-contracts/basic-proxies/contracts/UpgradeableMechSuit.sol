130c0f27b8ff1b0f60f210810b282a8fd51acaf3
zuberOne <zuberluk@proton.me>
2024-02-15 18:33:21 +0100

    mechsuit 1

--- campaigns/proxy-contracts/basic-proxies/contracts/UpgradeableMechSuit.sol
+++ campaigns/proxy-contracts/basic-proxies/contracts/UpgradeableMechSuit.sol
@@ -2,13 +2,107 @@
 pragma solidity ^0.8.19;
 
 contract UpgradeableMechSuit {
-    
+
+    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+    bytes32 internal constant _ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+
+    modifier onlyAdmin {
+
+        require(msg.sender == StorageSlot.getAddressSlot(_ADMIN_SLOT).value );
+        _;
+    }
+
+
     /// @notice Constructs the contract
     /// @param _implementation Address of logic contract to be linked
-    constructor(address _implementation) { }
+    constructor(address _implementation) { 
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = _implementation;
+        StorageSlot.getAddressSlot(_ADMIN_SLOT).value = msg.sender;
+        }
 
     /// @notice Upgrades contract by updating the linked logic contract
     /// @param _implementation Address of new logic contract to be linked
-    function upgradeTo(address _implementation) external { }
+    function upgradeTo(address _implementation) external onlyAdmin() {
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = _implementation;
+     }
 
+    fallback() external payable {
+
+        assembly {
+
+            let ptr := mload(0x40)
+
+            let _impl := sload(0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc)
+
+            // (1) copy incoming calldata into memory
+            calldatacopy(ptr, 0, calldatasize())
+
+            // (2) forward call to implementation contract
+            let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
+
+            // (3) retrieve return data
+            returndatacopy(ptr, 0, size)
+
+            // (4) forward return data back to caller
+            switch result
+            case 0 { revert(ptr, size) }  // Revert if call failed
+            default { return(ptr, size) } // Return if call succeeded
+        }
+    }
+
+}
+
+library StorageSlot {
+    struct AddressSlot {
+        address value;
+    }
+
+    struct BooleanSlot {
+        bool value;
+    }
+
+    struct Bytes32Slot {
+        bytes32 value;
+    }
+
+    struct Uint256Slot {
+        uint256 value;
+    }
+
+    /**
+     * @dev Returns an `AddressSlot` with member `value` located at `slot`.
+     */
+    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `BooleanSlot` with member `value` located at `slot`.
+     */
+    function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `Bytes32Slot` with member `value` located at `slot`.
+     */
+    function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `Uint256Slot` with member `value` located at `slot`.
+     */
+    function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
 }
