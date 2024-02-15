27b828d7b1b3e3646061666f349062d2b8e0123f
zuberOne <zuberluk@proton.me>
2024-02-15 20:51:52 +0100

    Download quest openzeppelin-upgrades

---
+++ campaigns/proxy-contracts/openzeppelin-upgrades/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require('@openzeppelin/hardhat-upgrades');
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.19"
+};
